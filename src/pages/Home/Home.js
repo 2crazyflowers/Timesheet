e6a548cee8359f46debd65e688b6f1d2d46aae16
Sara Bracewell <bracewell.sara@gmail.com>
2018-10-05 12:11:05 -0500

    grabbing user information

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import Login from "./Login";
+import LoginForm from "./LoginForm";
 import "./Home.css";
 
 class Home extends Component {
 
     state = {
+        users: [],
         username: "",
         password: "",
         // credentials: [],
@@ -27,37 +28,36 @@ class Home extends Component {
         .catch(err => console.log('there is an error with getUsers: ', err))
     }
 
-
+    // Keep track of what user enters for username so that input can be grabbed later
     handleUsernameChange = (event) => {
-        this.setState({ username: event.target.value })
+        this.setState({ username: event.target.value });
+        console.log('username: ', this.state.username);
     }
 
     handlePasswordChange = (event) => {
-        this.setState({ password: event.target.value })
+        this.setState({ password: event.target.value });
+        console.log('password: ', this.state.password);
     }
 
-    handleUserSubmit= (event) => {
+    handleUserSubmit = (event) => {
         event.preventDefault();
         console.log('Adding user info');
         console.log('this.state.username: ', this.state.username);
         console.log('this.state.password: ', this.state.password);
     }
 
-
-
-
     render() {
-
         return (
             <div className="Home">
                 <div>
                     <h1 className="Home-title">RS/Timesheet</h1>
                 </div>
                 <div>
-                    <Login
+                    <LoginForm
+                    users={this.state.users}
+                    handleUserSubmit = {this.handleUserSubmit}
                     handleUsernameChange = {this.handleUsernameChange}
                     handlePasswordChange = {this.handlePasswordChange}
-                    handleUserSubmit = {this.handleUserSubmit}
                     />
                 </div>
             </div>
---
+++ src/pages/Home/LoginForm.js
@@ -0,0 +1,49 @@
+// Importing React since we are using React.
+import React, { Component } from "react";
+
+
+class LoginForm extends Component {
+  state = {
+    value: '',
+  };
+
+
+  render() {
+    // const { users } = this.props;
+    return (
+      <div>
+        <form>
+          <label>
+          User
+          <input 
+          id='username'
+          type="text" 
+          name="username"  
+          className="Login"
+          value={this.props.username}
+          onChange={this.props.handleUsernameChange}/>
+          </label>
+          <br></br>
+          <label>
+          Pass
+          <input
+          id='password' 
+          type="text" 
+          name="pass"  
+          className="Login"
+          value={this.props.password}
+          onChange={this.props.handlePasswordChange}
+          />
+          </label>
+          <br></br>
+          <input type="submit" value="Login" className="Button"
+          onClick={this.props.handleUserSubmit}/>
+        </form>
+      </div>
+    );
+  }
+}
+
+// Exporting the LoginForm component
+// so that the App.js file can render the LoginForm page.
+export default (LoginForm);
